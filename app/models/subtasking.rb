6f14c04d59dbce4e4103632efc09a260ace75e42
Joseph Burke <joseph_burke@outlook.com>
2021-05-15 15:13:36 +0100

    Call accepts_nested_attributes in models.

--- app/models/subtasking.rb
+++ app/models/subtasking.rb
@@ -1,4 +1,6 @@
 class Subtasking < ApplicationRecord
   belongs_to :subtask, class_name: "Task"
   belongs_to :supertask, class_name: "Task"
+
+  accepts_nested_attributes_for :subtask, :supertask
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,11 @@ class Task < ApplicationRecord
 
   has_many :supertask_subtaskings, foreign_key: :subtask_id, class_name: "Subtasking", dependent: :destroy
   has_many :supertasks, through: :supertask_subtaskings
+
+  accepts_nested_attributes_for :subtask_subtaskings, :supertask_subtaskings
+
+  def build_sub_and_super_subtaskings
+    [self.subtask_subtaskings, self.supertask_subtaskings].each { |foo| foo.build }
+  end
+
 end
